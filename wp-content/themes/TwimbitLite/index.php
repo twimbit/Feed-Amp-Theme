e8571370ad93c286e82c6224448ca0a73bdc265f
siddhant <siddhant7847m@gmail.com>
2019-06-15 17:47:11 +0530

    header insight update

--- wp-content/themes/TwimbitLite/index.php
+++ wp-content/themes/TwimbitLite/index.php
@@ -46,12 +46,22 @@ $get_event = get_posts($event_args);
 
 <!-- Insights section -->
 <section class="features123  cid-r8J9eA34T9" id="features1-2f">
-	<div class="container">
-		<div class="title-wrap mbr-pb-4">
-			<h3 class="mbr-section-title mbr-bold mbr-fonts-style display-2">Insights</h3>
+    <div class="container">
+        <div class="title-wrap mbr-pb-4">
+            <h3 class="mbr-section-title mbr-bold mbr-fonts-style display-2">Insights</h3>
 
-		</div>
-		<div class="mbr-row mbr-jc-c">
+        </div>
+        <div class="mbr-row mbr-jc-c">
+            <figure class="ampstart-image-with-heading  m0 relative mb4">
+                <amp-img src="<?php print content_url() . '/themes/TwimbitLite/src/cat.jpg'; ?>" width="500" height="500" layout="responsive" alt="Beautiful Image of a rectangle"></amp-img>
+                <figcaption class="absolute right-0 bottom-0 left-0">
+                    <header class="ampstart-image-heading px2 py2 line-height-4">
+                        <h1>Views from Space</h1>
+                    </header>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="mbr-row mbr-jc-c">
 			<?php
 			foreach ($get_post as $val) {
 				$post_img = get_the_post_thumbnail_url($val);
@@ -59,91 +69,91 @@ $get_event = get_posts($event_args);
 				$post_title = get_the_title($val);
 				$post_excerpt = get_the_excerpt($val);
 				?>
-				<div class="card mbr-col-sm-12 mbr-col-md-8 mbr-col-lg-4 md-pb">
-					<div class="card-wrapper mbr-column">
-						<div class="card-img mbr-pb-3">
-							<amp-img src="<?php print $post_img; ?>" layout="responsive" width="348.5446009389671" height="232" alt="" class="placeholder-loader">
-								<div placeholder="" class="placeholder">
-									<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
-										<circle class="big" fill="none" stroke="#c2e0e0" stroke-width="3" stroke-dasharray="230" stroke-dashoffset="230" cx="150" cy="150" r="145"></circle>
-										<circle class="small" fill="none" stroke="#c2e0e0" stroke-width="3" stroke-dasharray="212" cx="150" cy="150" r="135"></circle>
-									</svg></div>
+                <div class="card mbr-col-sm-12 mbr-col-md-8 mbr-col-lg-4 md-pb">
+                    <div class="card-wrapper mbr-column">
+                        <div class="card-img mbr-pb-3">
+                            <amp-img src="<?php print $post_img; ?>" layout="responsive" width="348.5446009389671" height="232" alt="" class="placeholder-loader">
+                                <div placeholder="" class="placeholder">
+                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
+                                        <circle class="big" fill="none" stroke="#c2e0e0" stroke-width="3" stroke-dasharray="230" stroke-dashoffset="230" cx="150" cy="150" r="145"></circle>
+                                        <circle class="small" fill="none" stroke="#c2e0e0" stroke-width="3" stroke-dasharray="212" cx="150" cy="150" r="135"></circle>
+                                    </svg></div>
 
-							</amp-img>
-						</div>
-						<div class="card-box">
-							<a href="<?php print $post_url; ?>">
-								<h3 class="card-title mbr-bold mbr-fonts-style display-6"><?php echo mb_strimwidth($post_title, 0, 50, '...'); ?></h3>
+                            </amp-img>
+                        </div>
+                        <div class="card-box">
+                            <a href="<?php print $post_url; ?>">
+                                <h3 class="card-title mbr-bold mbr-fonts-style display-6"><?php echo mb_strimwidth($post_title, 0, 50, '...'); ?></h3>
 
-								<p class="card-text mbr-fonts-style mbr-pt-2 display-7"><?php echo $post_excerpt; ?></p>
-							</a>
-						</div>
-					</div>
-				</div>
+                                <p class="card-text mbr-fonts-style mbr-pt-2 display-7"><?php echo $post_excerpt; ?></p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
 			<?php } ?>
 			<?php if (count($get_post) >= 3) {
 				?>
 
-				<div class="mbr-row mbr-jc-c">
-					<div class="card-btn mbr-section-btn mbr-pt-2"><a class="btn btn-primary display-7" href="#">Read More</a></div>
-				</div>
+                <div class="mbr-row mbr-jc-c">
+                    <div class="card-btn mbr-section-btn mbr-pt-2"><a class="btn btn-primary display-7" href="#">Read More</a></div>
+                </div>
 			<?php } ?>
 
-		</div>
-	</div>
+        </div>
+    </div>
 </section>
 
 
 <!-- Stories -->
 <section class="features3 cid-r8TZVo6I8x" id="features3-2d">
-	<div class="container">
-		<div class="title mbr-pb-4">
-			<h3 class="mbr-section-title mbr-bold mbr-fonts-style display-2">Stories</h3>
-		</div>
+    <div class="container">
+        <div class="title mbr-pb-4">
+            <h3 class="mbr-section-title mbr-bold mbr-fonts-style display-2">Stories</h3>
+        </div>
 
 
 
-		<amp-carousel height="500" layout="fixed-height" type="carousel">
+        <amp-carousel height="500" layout="fixed-height" type="carousel">
 			<?php
 			foreach ($get_post_for_story as $val) {
 				$story_img = get_the_post_thumbnail_url($val);
 				$story_url = get_the_permalink($val);
 				$story_title = get_the_title($val);
 				?>
-				<div class="card mbr-col-sm-12 mbr-col-md-8 mbr-col-lg-4 md-pb">
-					<div class="card-wrapper mbr-column">
-						<div class="card-img mbr-flex">
-							<amp-img src="<?php print $story_img; ?>" layout="responsive" width="348.5446009389671" height="350" alt="" class="placeholder-loader">
-								<div placeholder="" class="placeholder">
-									<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
-										<circle class="big" fill="none" stroke="#c2e0e0" stroke-width="3" stroke-dasharray="230" stroke-dashoffset="230" cx="150" cy="150" r="145"></circle>
-										<circle class="small" fill="none" stroke="#c2e0e0" stroke-width="3" stroke-dasharray="212" cx="150" cy="150" r="135"></circle>
-									</svg></div>
+                <div class="card mbr-col-sm-12 mbr-col-md-8 mbr-col-lg-4 md-pb">
+                    <div class="card-wrapper mbr-column">
+                        <div class="card-img mbr-flex">
+                            <amp-img src="<?php print $story_img; ?>" layout="responsive" width="348.5446009389671" height="350" alt="" class="placeholder-loader">
+                                <div placeholder="" class="placeholder">
+                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
+                                        <circle class="big" fill="none" stroke="#c2e0e0" stroke-width="3" stroke-dasharray="230" stroke-dashoffset="230" cx="150" cy="150" r="145"></circle>
+                                        <circle class="small" fill="none" stroke="#c2e0e0" stroke-width="3" stroke-dasharray="212" cx="150" cy="150" r="135"></circle>
+                                    </svg></div>
 
-							</amp-img>
-						</div>
-						<div class="card-box mbr-m-auto mbr-pt-3 mbr-pb-3 mbr-px-4">
-							<a href="<?php print $story_url; ?>">
-								<h3 class="card-title mbr-bold mbr-fonts-style display-5"><?php print $story_title; ?>
-							</a></h3>
-						</div>
-					</div>
-				</div>
+                            </amp-img>
+                        </div>
+                        <div class="card-box mbr-m-auto mbr-pt-3 mbr-pb-3 mbr-px-4">
+                            <a href="<?php print $story_url; ?>">
+                                <h3 class="card-title mbr-bold mbr-fonts-style display-5"><?php print $story_title; ?>
+                            </a></h3>
+                        </div>
+                    </div>
+                </div>
 			<?php } ?>
-		</amp-carousel>
+        </amp-carousel>
 
 
-	</div>
+    </div>
 </section>
 
 <!-- Events -->
 <section class="features3 cid-r8J9eA34T9" id="features3-2d">
-	<div class="container">
-		<div class="title mbr-pb-4">
-			<h3 class="mbr-section-title mbr-bold mbr-fonts-style display-2">Latest Events</h3>
+    <div class="container">
+        <div class="title mbr-pb-4">
+            <h3 class="mbr-section-title mbr-bold mbr-fonts-style display-2">Latest Events</h3>
 
-		</div>
-		<div class="mbr-row mbr-jc-c">
+        </div>
+        <div class="mbr-row mbr-jc-c">
 			<?php
 			foreach ($get_post as $val) {
 				$event_img = get_the_post_thumbnail_url($val);
@@ -151,34 +161,34 @@ $get_event = get_posts($event_args);
 				$event_title = get_the_title($val);
 				$event_excerpt = get_the_excerpt($val);
 				?>
-				<div class="card mbr-col-sm-12 mbr-col-md-8 mbr-col-lg-4 md-pb">
-					<div class="card-wrapper mbr-column">
-						<div class="card-img mbr-flex">
-							<amp-img src="<?php echo $event_img; ?>" layout="responsive" width="348.5446009389671" height="232" alt="" class="placeholder-loader">
-								<div placeholder="" class="placeholder">
-									<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
-										<circle class="big" fill="none" stroke="#c2e0e0" stroke-width="3" stroke-dasharray="230" stroke-dashoffset="230" cx="150" cy="150" r="145"></circle>
-										<circle class="small" fill="none" stroke="#c2e0e0" stroke-width="3" stroke-dasharray="212" cx="150" cy="150" r="135"></circle>
-									</svg></div>
+                <div class="card mbr-col-sm-12 mbr-col-md-8 mbr-col-lg-4 md-pb">
+                    <div class="card-wrapper mbr-column">
+                        <div class="card-img mbr-flex">
+                            <amp-img src="<?php echo $event_img; ?>" layout="responsive" width="348.5446009389671" height="232" alt="" class="placeholder-loader">
+                                <div placeholder="" class="placeholder">
+                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
+                                        <circle class="big" fill="none" stroke="#c2e0e0" stroke-width="3" stroke-dasharray="230" stroke-dashoffset="230" cx="150" cy="150" r="145"></circle>
+                                        <circle class="small" fill="none" stroke="#c2e0e0" stroke-width="3" stroke-dasharray="212" cx="150" cy="150" r="135"></circle>
+                                    </svg></div>
 
-							</amp-img>
-						</div>
-						<div class="card-box mbr-m-auto mbr-pt-3 mbr-pb-3 mbr-px-4">
-							<h3 class="card-title mbr-bold mbr-fonts-style display-6"><?php echo mb_strimwidth($event_title, 0, 50, '...'); ?></h3>
+                            </amp-img>
+                        </div>
+                        <div class="card-box mbr-m-auto mbr-pt-3 mbr-pb-3 mbr-px-4">
+                            <h3 class="card-title mbr-bold mbr-fonts-style display-6"><?php echo mb_strimwidth($event_title, 0, 50, '...'); ?></h3>
 
-							<p class="card-text mbr-fonts-style mbr-pt-2 display-7"><?php echo $event_excerpt; ?></p>
+                            <p class="card-text mbr-fonts-style mbr-pt-2 display-7"><?php echo $event_excerpt; ?></p>
 
-						</div>
-					</div>
-				</div>
+                        </div>
+                    </div>
+                </div>
 			<?php } ?>
-		</div>
+        </div>
 		<?php if (count($get_post) >= 3) {
 			?>
 
-			<div class="mbr-row mbr-jc-c">
-				<div class="card-btn mbr-section-btn mbr-pt-2"><a class="btn btn-primary display-7" href="#">Read More</a></div>
-			</div>
+            <div class="mbr-row mbr-jc-c">
+                <div class="card-btn mbr-section-btn mbr-pt-2"><a class="btn btn-primary display-7" href="#">Read More</a></div>
+            </div>
 		<?php } ?>
-	</div>
+    </div>
 </section>
