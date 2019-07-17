d307da6c941ed2d6127d85338b2dc1d0900f9c21
siddhant <siddhant7847m@gmail.com>
2019-07-17 11:17:12 +0530

    archive card label added

--- wp-content/themes/TwimbitLite/archive.php
+++ wp-content/themes/TwimbitLite/archive.php
@@ -55,7 +55,7 @@ $get_sub_cat = get_posts($args);
 <?php //print_r(get_the_category());
 ?>
 <!-- Remaining categories -->
-<section id="cards-feed" class="mb4">
+<section id="cards-feed" class="mb4 row">
     <div class="container mt2" style="margin-bottom: 6rem;">
         <div class="lg-col-7 md-col-6 sm-col-7 xs-col-12">
             <?php
@@ -76,10 +76,57 @@ $get_sub_cat = get_posts($args);
 
                             </div>
                         </a>
+                        <div class="feed-action">
+                            <div class="feed-button">
+                                <div class="feed-wrap">
+                                    <div class="feed-button-in">
+                                        <div class="atomic-heart">
+                                            <?php if (get_post_type($val) == "post") { ?>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="41.817" height="37.171" viewBox="0 0 41.817 37.171">
+                                                    <path class="a" d="M3,31.878H14.616V27.232H3Zm15.1,0H29.716V27.232H18.1Zm15.1,0H44.817V27.232H33.2ZM3,41.171H7.646V36.524H3Zm9.293,0h4.646V36.524H12.293Zm9.293,0h4.646V36.524H21.585Zm9.293,0h4.646V36.524H30.878Zm9.293,0h4.646V36.524H40.171ZM3,22.585H21.585V17.939H3Zm23.232,0H44.817V17.939H26.232ZM3,4v9.293H44.817V4Z" transform="translate(-3 -4)" />
+                                                </svg>
+                                            <?php
+                                            } else if (get_post_type($val) == "video") { ?>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="41.817" height="37.171" viewBox="0 0 49.131 49.131">
+                                                    <path class="a" d="M26.566,2A24.566,24.566,0,1,0,51.131,26.566,24.574,24.574,0,0,0,26.566,2ZM21.652,37.62V15.511L36.392,26.566Z" transform="translate(-2 -2)" />
+                                                </svg>
+                                            <?php
+                                            } else if (get_post_type($val) == "podcast") { ?>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="41.817" height="37.171" viewBox="0 0 48.491 39.675">
+                                                    <path class="a" d="M45.083,3H5.408A4.421,4.421,0,0,0,1,7.408V38.266a4.421,4.421,0,0,0,4.408,4.408H45.083a4.421,4.421,0,0,0,4.408-4.408V7.408A4.421,4.421,0,0,0,45.083,3Zm0,35.266H5.408V7.408H45.083ZM16.429,29.45a6.568,6.568,0,0,1,8.817-6.216V9.612H36.266v4.408H29.654v15.5a6.613,6.613,0,0,1-13.225-.066Z" transform="translate(-1 -3)" />
+                                                </svg>
+                                            <?php
+                                            } else if (get_post_type($val) == "amp_story") { ?>
+                                                <svg id="amp-stories" viewBox="0 0 36 32" style="    transform: translate(0.5px, 2px) scale(0.6);">
+                                                    <path d="M7.111 0h21.333v32h-21.333v-32zM9.481 2.37v27.259h16.593v-27.259h-16.593zM0 4.741h2.37v22.519h-2.37v-22.519zM33.185 4.741h2.37v22.519h-2.37v-22.519z"></path>
+                                                </svg>
 
+                                            <?php
+                                            } ?>
+                                        </div>
+                                    </div>
+                                    <div class="count">
+                                        <?php if (get_post_type($val) == "post") {
+                                            print "Insight";
+                                        } else if (get_post_type($val) == "video") {
+                                            print "Video";
+                                        } else if (get_post_type($val) == "podcast") {
+                                            print "Podcast";
+                                        } else if (get_post_type($val) == "amp_story") {
+                                            print "Story";
+                                        }  ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             <?php } ?>
+        </div>
+    </div>
+    <div class="container">
+        <div class="lg-col-4 md-col-5 sm-col-6 xs-col-12">
+            hello
         </div>
     </div>
 </section>
