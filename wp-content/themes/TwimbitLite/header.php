25b12f966de43904f97a494fbfddee85bd289cfe
aman_razdan <aman111334@gmail.com>
2019-07-24 17:41:05 +0530

    fixed nav selector

--- wp-content/themes/TwimbitLite/header.php
+++ wp-content/themes/TwimbitLite/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html amp="1.2.1" <?php language_attributes(); ?> amp-version="1907161745080">
+7<html amp="1.2.1" <?php language_attributes(); ?> amp-version="1907161745080">
 
 <head>
     <!-- AMP Scripts -->
@@ -3801,6 +3801,7 @@
 
         .active-nav {
             color: #f16c70;
+            border-bottom: 3px solid #f16c70;
         }
 
         .search-lightbox {
