549de37030cc138be7590344fae1bf189fe51c57
[shalini bose] <shalinibose25@gmail.com>
2019-07-30 10:29:18 +0530

    added single poll page

---
+++ wp-content/themes/TwimbitLite/single-poll.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html amp="">
+
+<head>
+
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <script async custom-element="amp-video" src="https://cdn.ampproject.org/v0/amp-video-0.1.js"></script>
+
+
+    <script async custom-element="amp-video-docking" src="https://cdn.ampproject.org/v0/amp-video-docking-0.1.js"></script>
+    <script async custom-element="amp-animation" src="https://cdn.ampproject.org/v0/amp-animation-0.1.js"></script>
+
+    <link rel="canonical" href="https://amp.dev/documentation/examples/multimedia-animations/advanced_video_docking/index.html">
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+    <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+
+    <script type="text/javascript" src="https://static.apester.com/js/sdk/latest/apester-sdk.js" async></script>
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <script async custom-element=”amp-brightcove” src=”https://cdn.ampproject.org/v0/amp-brightcove-0.1.js”></script>
+    <script async custom-element="amp-iframe" src="https://cdn.ampproject.org/v0/amp-iframe-0.1.js"></script>
+
+    <style amp-custom>
+        body {
+            margin: 0;
+            background: white;
+            font-family: sans-serif;
+        }
+        .main
+        {
+            text-align:center;
+            margin-top: 5em;
+        }
+
+
+        </style>
+
+
+
+</head>
+
+<body>
+	<div class="apester-shortcode-replacement main" contenteditable="false">
+		<div id="apester-media-5d3ed066b235840e08a8d509" class="apester-media-wrapper" data-mce-placeholder="1">
+			<iframe id="apester-media-5d3ed066b235840e08a8d509" class="apester-shortcode-iframe" contenteditable="false" src="https://renderer.apester.com/interaction/5d3ed066b235840e08a8d509" data-apester-media-id="5d3ed066b235840e08a8d509" data-mce-placeholder="1" data-mce-fragment="1" width="600" height="350">
+
+			</iframe>
+		</div>
+		<div class="apester-media-overlay"> </div>
+	</div>
+
+<br><br>
+<div class="apester-shortcode-replacement" contenteditable="false" style="margin-left:2em;">
+    <div id="apester-media-5d3ea91a977f5336e78543ec" class="apester-media-wrapper" data-mce-placeholder="1">
+        <iframe id="apester-media-5d3ea91a977f5336e78543ec" class="apester-shortcode-iframe" contenteditable="false" src="https://renderer.apester.com/interaction/5d3ea91a977f5336e78543ec" data-apester-media-id="5d3ea91a977f5336e78543ec" data-mce-placeholder="1" width="600" height="350"></iframe>
+    </div>
+    <div class="apester-media-overlay"> </div>
+</div>
+<div class="apester-shortcode-replacement" contenteditable="false" style="margin-left:20em;">
+    <div id="apester-media-5d3eccea977f53275d854414" class="apester-media-wrapper" data-mce-placeholder="1"><iframe id="apester-media-5d3eccea977f53275d854414" class="apester-shortcode-iframe" contenteditable="false" src="https://renderer.apester.com/interaction/5d3eccea977f53275d854414" data-apester-media-id="5d3eccea977f53275d854414" data-mce-placeholder="1" data-mce-fragment="1"  width="600" height="350"></iframe></div>
+    <div class="apester-media-overlay"> </div>
+</div>
+
+   
+
+    <amp-video controls
+               width="640"
+               height="360"
+               layout="responsive"
+               poster="/static/inline-examples/images/kitten-playing.png">
+        <source src="/static/inline-examples/videos/kitten-playing.webm"
+                type="video/webm" />
+        <source src="/static/inline-examples/videos/kitten-playing.mp4"
+                type="video/mp4" />
+        <div fallback>
+            <p>This browser does not support the video element.</p>
+        </div>
+    </amp-video>
+
+
+</body>
+</html>
