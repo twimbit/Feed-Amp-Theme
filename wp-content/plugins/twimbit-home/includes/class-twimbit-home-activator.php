2eb60aaec66f32aea5243ab0d9be460771fbdf03
siddhant <siddhant7847m@gmail.com>
2019-11-11 16:33:39 +0530

    twimcast-api plugin added

---
+++ wp-content/plugins/twimbit-home/includes/class-twimbit-home-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://twimbit.com/
+ * @since      1.0.0
+ *
+ * @package    Twimbit_Home
+ * @subpackage Twimbit_Home/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Twimbit_Home
+ * @subpackage Twimbit_Home/includes
+ * @author     twimbit <reachus@twimbit.com>
+ */
+class Twimbit_Home_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
