63dcdb5fd421397f88f7186b832cb31a717ac84e
siddhant <siddhant7847m@gmail.com>
2019-12-01 23:13:07 +0530

    archive pahe added

--- wp-content/themes/Twimcast/index.php
+++ wp-content/themes/Twimcast/index.php
@@ -6,45 +6,48 @@
 			<?php get_template_part('templates/twimcast', 'sidebar'); ?>
 		</div>
 	</section>
-	<section id="main-widget-area" class="widget-area-section">
-		<?php
-		$widgets = get_field('add_widgets', 'options'); ?>
-		<div class="custom-carousel amp-carousel-style explore-all">
-			<?php get_template_part('widgets/widget', 'custom'); ?>
-		</div>
+	<section class="widget-area">
+		<div id="main-widget-area" class="widget-area-section">
+			<?php //$widgets = get_field('add_widgets', 'options'); 
+			?>
+			<div class="custom-carousel amp-carousel-style explore-all">
+				<?php get_template_part('widgets/widget', 'custom'); ?>
+			</div>
 
-		<div class="featured-widget amp-carousel-style explore-all">
-			<?php get_template_part('widgets/widget', 'featured'); ?>
-		</div>
+			<div class="featured-widget amp-carousel-style explore-all">
+				<?php get_template_part('widgets/widget', 'featured'); ?>
+			</div>
 
-		<div class="trending-widget explore-all">
-			<?php get_template_part('widgets/widget', 'trending'); ?>
-		</div>
+			<div class="trending-widget explore-all">
+				<?php get_template_part('widgets/widget', 'trending'); ?>
+			</div>
 
-		<div class="trending-widget explore-all">
-			<?php get_template_part('widgets/widget', 'latest'); ?>
-		</div>
+			<div class="trending-widget explore-all">
+				<?php get_template_part('widgets/widget', 'latest'); ?>
+			</div>
 
-		<div class="trending-widget explore-all">
-			<?php get_template_part('widgets/widget', 'banking'); ?>
-		</div>
+			<div class="trending-widget explore-all">
+				<?php get_template_part('widgets/widget', 'banking'); ?>
+			</div>
 
-		<div class="trending-widget explore-all">
-			<?php get_template_part('widgets/widget', 'digital'); ?>
-		</div>
+			<div class="trending-widget explore-all">
+				<?php get_template_part('widgets/widget', 'digital'); ?>
+			</div>
 
-		<div class="suggested-widget explore-all">
-			<?php get_template_part('widgets/widget', 'suggested'); ?>
-		</div>
+			<div class="suggested-widget explore-all">
+				<?php get_template_part('widgets/widget', 'suggested'); ?>
+			</div>
 
-		<div class="suggested-widget explore-all" style="max-width:900px">
-			<?php get_template_part('widgets/widget', 'report'); ?>
-		</div>
+			<div class="suggested-widget explore-all" style="max-width:900px">
+				<?php get_template_part('widgets/widget', 'report'); ?>
+			</div>
 
-		<div class="twimcast-text">
-			<h2>TwimCast</h2>
+			<div class="twimcast-text">
+				<h2>TwimCast</h2>
+			</div>
 		</div>
 	</section>
+
 </main><!-- #site-content -->
 
 <?php
